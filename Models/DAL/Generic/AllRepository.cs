bff42ed5567d11f0ca79bd367106da9606772ae7
Mohamad Jaber <MJaber.c@stcpay.com.sa>
2023-05-12 15:06:41 +0300

    Implementing generic DAL

---
+++ Models/DAL/Generic/AllRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreWebApp.Models.DAL.Generic
+{
+    public class AllRepository<T> : IAllRepository<T> where T : class
+    {
+        private readonly MainDbContext _dataContext;
+        private readonly DbSet<T> _dbEntity;
+
+        public AllRepository(MainDbContext dataContext)
+        {
+            _dataContext = dataContext;
+            _dbEntity = _dataContext.Set<T>();
+        }
+
+        public void DeleteModel(T modelId)
+        {
+            var model = _dbEntity.Find(modelId);
+            _dbEntity.Remove(model);
+        }
+
+        public IEnumerable<T> GetModel()
+        {
+            return _dbEntity.ToList();
+        }
+
+        public T GetModelById(int modelId)
+        {
+            return _dbEntity.Find(modelId);
+        }
+
+        public void Save()
+        {
+            _dataContext.SaveChanges();
+        }
+
+        public void UpdateModel(T model)
+        {
+            _dataContext.Entry(model).State = EntityState.Modified;
+        }
+    }
+}
---
+++ Models/DAL/Generic/IAllRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace EFCoreWebApp.Models.DAL.Generic
+{
+    public interface IAllRepository<T> where T : class
+    {
+        IEnumerable<T> GetModel();
+        T GetModelById(int modelId);
+        void UpdateModel (T model);
+        void DeleteModel (T modelId);
+        void Save();
+    }
+}
