fc75231691eec51e51855d167f76bb606d35565d
Mohamad Jaber <MJaber.c@stcpay.com.sa>
2023-05-12 14:42:31 +0300

    Implementing non generic DAL layer

--- Models/CustomerDto.cs
+++ Models/CustomerDto.cs
@@ -1,10 +1,14 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace EFCoreWebApp.Models
 {
     public class CustomerDto
     {
+        [Required]
         public string FirstName { get; set; } = null!;
 
+        [Required]
         public string LastName { get; set; } = null!;
     }
 }
