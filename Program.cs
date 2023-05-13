0c677a376f092b224e45bf92453e7229220dac1f
Mohamad Jaber <MJaber.c@stcpay.com.sa>
2023-05-13 12:43:42 +0300

    adding hangfire

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-using EFCoreWebApp.Models;
+using Hangfire;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+///Hangfire config
+builder.Services.AddHangfire(x => x.UseSqlServerStorage(builder.Configuration.GetConnectionString("HangfireDB")));
+builder.Services.AddHangfireServer();  //the server which processes the jobs
 
 
 ///adding Db Context injection  //no need for DI since the instance was initiated in the Repository
@@ -24,6 +29,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+///Hangfire config
+app.UseHangfireDashboard();
 
 app.UseHttpsRedirection();
 
